81396441a07467f7d0bcc9dc4e13d1f074f9fba8
U-fabulord\jb <wozgonon@gmail.com>
2018-05-11 12:05:52 +0100

    All compiles now: includes webmercator, zoom and ramer douglas simplifier

---
+++ bin/webmap.rs
@@ -0,0 +1,39 @@
+extern crate libwebmap;
+
+use libwebmap::webmap::LonLat;
+use libwebmap::webmap::WebMap;
+use libwebmap::webmap::WebMercator;
+use libwebmap::webmap::RamerDouglasPeuker;
+use libwebmap::webmap::PerpendicularDistance;
+
+fn main() {
+
+    let projection = WebMercator {};
+    let zoom = 18;
+    let webmap = WebMap::new (zoom, Box::new(projection));
+    let epsilon = 0.5;
+    let metric = PerpendicularDistance {};
+    let simplifier = RamerDouglasPeuker::new (epsilon, Box::new (metric));
+    for nn in 0..20 {
+        let sides = 1 << nn;
+        let polygon = LonLat::make_regular_convex_polygon (sides);
+    	let simplified_polygon = webmap.simplify_lonlats (&polygon, &simplifier);
+    	println!("Polygon points: {} simplified to: {}", polygon.len (), simplified_polygon.len ());
+    }
+}
+
+
+//  GeoJson classes
+
+//struct Polygon {
+//    outer : LonLats,
+//    inner : LonLats []
+//}
+
+//struct MultiLine {
+    //   points : LonLats []
+//}
+
+//struct MultiPolygon {
+    //polygons : Polygon [];
+//}
